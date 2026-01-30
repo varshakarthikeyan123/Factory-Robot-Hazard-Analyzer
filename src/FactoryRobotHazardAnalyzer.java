9dd89bd3ff5246f37c4749fbc03da1d74e5b0e0e
varshakarthikeyan123 <varshakarthikeyan1@gmail.com>
2026-01-30 16:48:22 +0530

    [Varsha]: Added: UC3: Refactored hazard calculation into auditor class

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class FactoryRobotHazardAnalyzer {
 
     public static void main(String[] args) {
@@ -7,28 +8,48 @@ public class FactoryRobotHazardAnalyzer {
 
         System.out.println("Factory Robot Hazard Analyzer");
 
-
         Scanner scanner = new Scanner(System.in);
-
 
         System.out.print("Enter Arm Precision: ");
         double armPrecision = scanner.nextDouble();
 
-
         System.out.print("Enter Worker Density: ");
         int workerDensity = scanner.nextInt();
 
-        scanner.nextLine(); // clear buffer
-
+        scanner.nextLine();
 
         System.out.print("Enter Machinery State: ");
         String machineryState = scanner.nextLine();
 
 
-        System.out.println("Arm Precision: " + armPrecision);
-        System.out.println("Worker Density: " + workerDensity);
-        System.out.println("Machinery State: " + machineryState);
+        RobotHazardAuditor auditor = new RobotHazardAuditor();
+        double hazardRiskScore = auditor.calculateHazardRisk(
+                armPrecision,
+                workerDensity,
+                machineryState
+        );
+
+
+        System.out.println("Robot Hazard Risk Score: " + hazardRiskScore);
 
         scanner.close();
+    }
+}
+
+
+class RobotHazardAuditor {
+
+    public double calculateHazardRisk(double armPrecision,
+                                      int workerDensity,
+                                      String machineryState) {
+
+        double riskScore = armPrecision * workerDensity;
+
+        if (machineryState.equalsIgnoreCase("CRITICAL")) {
+            riskScore = riskScore * 1.5;
+        }
+
+
+        return riskScore;
     }
 }
